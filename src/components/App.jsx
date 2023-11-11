b89451f4950eebd894f004665d2793f1e385242c
Miguel <ohyeahs4@hotmail.com>
2023-11-11 12:40:31 -0500

    Router Agregado

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import Layout from "../Layout";
 import UserList from "../pages/UserList";
 import { Container } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom" 
 
 const App = () => {
   return (
-    <Container maxHeight="100vh" maxWidth="100vh" sx={{ background: "#ECDCC5" }}>
+    <Router>
+      <Container maxHeight="100vh" maxWidth="100vh" sx={{ background: "#ECDCC5" }}>
       <Layout>
-        <UserList />
+        <Routes>
+          <Route path ="/" element ={<UserList />}/>
+        </Routes>
       </Layout>
     </Container>
+    </Router>
   );
 };
 
